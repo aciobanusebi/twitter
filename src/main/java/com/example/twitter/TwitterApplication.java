811f6e8f743a8475d6cbb50bc07354720e8dac06
Sebastian Ciobanu <aciobanusebi@gmail.com>
2022-04-13 20:56:20 +0300

    Deleted comments in main

--- src/main/java/com/example/twitter/TwitterApplication.java
+++ src/main/java/com/example/twitter/TwitterApplication.java
@@ -13,17 +13,6 @@ public class TwitterApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(TwitterApplication.class, args);
-
-//        UserRepository repo = context.getBean(UserRepository.class);
-//
-//        User user = new User();
-//        user.setId("acsebi");
-//        user.setFirstName("Sebastian");
-//        user.setLastName("Ciobanu");
-//        user.setEmail("aciobanusebi@gmail.com");
-//        user.setPassword("pass");
-//        user = repo.save(user);
-//        System.out.println(repo.findAll());
     }
 
 }
